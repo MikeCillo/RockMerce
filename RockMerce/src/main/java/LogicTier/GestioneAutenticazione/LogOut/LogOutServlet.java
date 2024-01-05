af2c8db2e23f3f8c88bf205042c31c12dbfafe9c
GionnyCuomo <g.cuomo36@studenti.unisa.it>
2024-01-05 15:37:27 +0100

    aggiunto Gestione Autenticazione e le relative interfacce e service

--- RockMerce/src/main/java/LogicTier/GestioneAutenticazione/LogOut/LogOutServlet.java
+++ RockMerce/src/main/java/LogicTier/GestioneAutenticazione/LogOut/LogOutServlet.java
@@ -1,27 +1,30 @@
-package LogicTier.GestioneGuitars.CatalogoClassic;
+package LogicTier.GestioneAutenticazione.LogOut;
 
-import LogicTier.Utils.GuitarService;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-@WebServlet(value = "/CatalogoClassicControl")
-public class CatalogoClassicControl extends HttpServlet {
+
+@WebServlet(value = "/LogOut-Servlet")
+
+public class LogOutServlet extends HttpServlet {
+
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String address ="/PresentationTier/GuitarsGUI/CatalogoGUI/GuitarShop.jsp";
 
-        GuitarService guitarService=new GuitarService();
-        request.setAttribute("guitars",guitarService.retrieveGuitarsByCategory("classic"));
+        HttpSession session=request.getSession();
+        session.removeAttribute("customer");
 
+        String address="index.jsp";
         RequestDispatcher dispatcher =
                 request.getRequestDispatcher(address);
         dispatcher.forward(request, response);
-    }
 
+    }
 }
